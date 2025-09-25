1e34508d6f6b836e688861f8729432ab1de48a7e
Purushottam Kumar <purushottam2fx@gmail.com>
2025-09-25 14:47:46 +0530

    add API patch and delete method

--- create_api.js
+++ create_api.js
@@ -17,6 +17,16 @@ app.put("/update_user/:id",(req,res)=>{
     //const {name,email}=req.body;
     res.status(200).json({"message":"updated","name":"jacky","email":"jacky@gmail.com","id":id});
 })
+
+app.patch("/patch_user/:id",(req,res)=>{
+    const id=req.params.id;
+    res.status(200).json({"message":"user data patched","id":id});
+})
+
+app.delete("/delete_user/:id",(req,res)=>{
+    const id=req.params.id;
+    res.status(200).json({"message":"user deleted","id":id});
+})
 app.listen(5421,(error)=>{
     console.log("error:-" + error);
 })
