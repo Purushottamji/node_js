48dce84bb82183526eb0f0a55e54bd35921ee6a8
Purushottam Kumar <purushottam2fx@gmail.com>
2025-09-25 13:49:40 +0530

    add api put method

--- create_api.js
+++ create_api.js
@@ -12,6 +12,11 @@ app.post("/create_user",(req,res)=>{
     res.status(201).json({"name":name,"email":email});
 })
 
+app.put("/update_user/:id",(req,res)=>{
+    const id=req.params.id;
+    //const {name,email}=req.body;
+    res.status(200).json({"message":"updated","name":"jacky","email":"jacky@gmail.com","id":id});
+})
 app.listen(5421,(error)=>{
     console.log("error:-" + error);
 })
